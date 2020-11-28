6b6e59dce22bb4f876788c2f8933401a84dae459
RealMVC <39596187+RealMVC@users.noreply.github.com>
2020-11-28 07:45:08 -0500

    Added Collapsing Headers and View Player Tasks

--- gui/tabs/radar_tab.cpp
+++ gui/tabs/radar_tab.cpp
@@ -4,9 +4,13 @@ namespace RadarTab {
 	void Render() {
 		if (ImGui::BeginTabItem("Radar")) {
 			ImGui::Checkbox("Show Radar", &State.ShowRadar);
-			ImGui::Checkbox("Show Dead Bodies", &State.ShowRadar_DeadBodies);
-			ImGui::Checkbox("Show Ghosts", &State.ShowRadar_Ghosts);
-			ImGui::Checkbox("Right Click to Teleport", &State.ShowRadar_RightClick_Teleport);
+			ImGui::NewLine();
+			if (ImGui::CollapsingHeader("Options"))
+			{
+				ImGui::Checkbox("Show Dead Bodies", &State.ShowRadar_DeadBodies);
+				ImGui::Checkbox("Show Ghosts", &State.ShowRadar_Ghosts);
+				ImGui::Checkbox("Right Click to Teleport", &State.ShowRadar_RightClick_Teleport);
+			}
 			ImGui::EndTabItem();
 		}
 	}
