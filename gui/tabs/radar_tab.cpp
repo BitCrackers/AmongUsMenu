0a5400c33107007fff6d5f83b6848e0834d8b74c
RealMVC <39596187+RealMVC@users.noreply.github.com>
2020-11-28 07:47:50 -0500

    Revert "Added Collapsing Headers and View Player Tasks"
    This reverts commit 6b6e59dce22bb4f876788c2f8933401a84dae459.

--- gui/tabs/radar_tab.cpp
+++ gui/tabs/radar_tab.cpp
@@ -4,13 +4,9 @@ namespace RadarTab {
 	void Render() {
 		if (ImGui::BeginTabItem("Radar")) {
 			ImGui::Checkbox("Show Radar", &State.ShowRadar);
-			ImGui::NewLine();
-			if (ImGui::CollapsingHeader("Options"))
-			{
-				ImGui::Checkbox("Show Dead Bodies", &State.ShowRadar_DeadBodies);
-				ImGui::Checkbox("Show Ghosts", &State.ShowRadar_Ghosts);
-				ImGui::Checkbox("Right Click to Teleport", &State.ShowRadar_RightClick_Teleport);
-			}
+			ImGui::Checkbox("Show Dead Bodies", &State.ShowRadar_DeadBodies);
+			ImGui::Checkbox("Show Ghosts", &State.ShowRadar_Ghosts);
+			ImGui::Checkbox("Right Click to Teleport", &State.ShowRadar_RightClick_Teleport);
 			ImGui::EndTabItem();
 		}
 	}
--- gui/tabs/self_tab.cpp
+++ gui/tabs/self_tab.cpp
@@ -3,44 +3,33 @@
 namespace SelfTab {
 	void Render() {
 		if (ImGui::BeginTabItem("Self")) {
+			ImGui::Checkbox("Max Vision", &State.MaxVision);
+			ImGui::Checkbox("Wallhack", &State.Wallhack);
 
-			if (ImGui::CollapsingHeader("Vision"))
+			if (ImGui::Checkbox("Freecam", &State.FreeCam))
 			{
-				ImGui::Checkbox("Max Vision", &State.MaxVision);
-				ImGui::Checkbox("Wallhack", &State.Wallhack);
-
-				if (ImGui::Checkbox("Freecam", &State.FreeCam))
-				{
-					State.FollowPlayer = false;
-					State.PlayerToFollow = **Game::pLocalPlayer;
-				}
-
-				SteppedSliderFloat("Freecam Speed", &State.FreeCamSpeed, 0.5f, 3.f, 0.25f, "%.2fx", ImGuiSliderFlags_Logarithmic | ImGuiSliderFlags_NoInput);
+				State.FollowPlayer = false;
+				State.PlayerToFollow = **Game::pLocalPlayer;
 			}
 
-			if (ImGui::CollapsingHeader("Chat"))
-			{
-				ImGui::Checkbox("Always show Chat Button", &State.ChatAlwaysActive);
-				ImGui::Checkbox("Read Messages by Ghosts", &State.ReadGhostMessages);
-			}
+			SteppedSliderFloat("Freecam Speed", &State.FreeCamSpeed, 0.5f, 3.f, 0.25f, "%.2fx", ImGuiSliderFlags_Logarithmic | ImGuiSliderFlags_NoInput);
 
-			if (ImGui::CollapsingHeader("Misc."))
-			{
-				ImGui::Checkbox("Reveal Impostors", &State.RevealImpostors);
-				ImGui::Checkbox("Unlock Vents", &State.UnlockVents);
-
-				if (ImGui::Checkbox("No Clip", &State.NoClip)) {
-					if (!IsInGame()) State.NoClip = false;
-					else {
-						if (!(GetPlayerData(*Game::pLocalPlayer)->fields.IsDead)) {
-							if (State.NoClip)
-								app::GameObject_set_layer(app::Component_get_gameObject((Component*)(*Game::pLocalPlayer), NULL), app::LayerMask_NameToLayer(convert_to_string("Ghost"), NULL), NULL);
-							else
-								app::GameObject_set_layer(app::Component_get_gameObject((Component*)(*Game::pLocalPlayer), NULL), app::LayerMask_NameToLayer(convert_to_string("Players"), NULL), NULL);
-						}
+			if (ImGui::Checkbox("No Clip", &State.NoClip)) {
+				if (!IsInGame()) State.NoClip = false;
+				else {
+					if (!(GetPlayerData(*Game::pLocalPlayer)->fields.IsDead)) {
+						if (State.NoClip)
+							app::GameObject_set_layer(app::Component_get_gameObject((Component*)(*Game::pLocalPlayer), NULL), app::LayerMask_NameToLayer(convert_to_string("Ghost"), NULL), NULL);
+						else
+							app::GameObject_set_layer(app::Component_get_gameObject((Component*)(*Game::pLocalPlayer), NULL), app::LayerMask_NameToLayer(convert_to_string("Players"), NULL), NULL);
 					}
 				}
 			}
+
+			ImGui::Checkbox("Reveal Impostors", &State.RevealImpostors);
+			ImGui::Checkbox("Unlock Vents", &State.UnlockVents);
+			ImGui::Checkbox("Always show Chat Button", &State.ChatAlwaysActive);
+			ImGui::Checkbox("Read Messages by Ghosts", &State.ReadGhostMessages);
 
 			ImGui::EndTabItem();
 		}
