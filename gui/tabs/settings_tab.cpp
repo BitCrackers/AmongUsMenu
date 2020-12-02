24f782dc4e13911485af8635e0eaab653655dc4d
RealMVC <perfect6number@gmail.com>
2020-12-02 15:28:17 -0500

    Bugfixes (#33)
    Fixed Issues:
    #27 Chat Window Disappearing
    #26 Radar Color Being Reset
    * Fixed 3 Warnings in _hooks.cpp
    * Fixed line

--- gui/tabs/settings_tab.cpp
+++ gui/tabs/settings_tab.cpp
@@ -7,7 +7,7 @@ namespace SettingsTab {
 			ImGui::Dummy(ImVec2(4, 4));
 			ImGui::Text("Change Theme Color:");
 			ImGui::Dummy(ImVec2(4, 4));
-			ImGui::PushItemWidth(ImGui::GetWindowWidth() / 3.5);
+			ImGui::PushItemWidth(ImGui::GetWindowWidth() / 3.5f);
 			ImGui::ColorPicker4("##picker", (float*)&State.SelectedColor, ImGuiColorEditFlags_NoInputs | ImGuiColorEditFlags_NoLabel | ImGuiColorEditFlags_PickerHueWheel);
 			ImGui::PopItemWidth();
 
