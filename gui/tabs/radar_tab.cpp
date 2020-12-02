baaa388db6b0243b7f06e49fe2ccd2fd262ad272
RealMVC <perfect6number@gmail.com>
2020-12-02 17:08:12 -0500

    Added Radar Opacity Slider and Reset Theme Color

--- gui/tabs/radar_tab.cpp
+++ gui/tabs/radar_tab.cpp
@@ -12,8 +12,15 @@ namespace RadarTab {
 
 			ImGui::Checkbox("Show Dead Bodies", &State.ShowRadar_DeadBodies);
 			ImGui::Checkbox("Show Ghosts", &State.ShowRadar_Ghosts);
-			ImGui::Checkbox("Hide Radar During Meetings", &State.HideRadar_During_Meetings);
 			ImGui::Checkbox("Right Click to Teleport", &State.ShowRadar_RightClick_Teleport);
+
+			ImGui::Dummy(ImVec2(7, 7));
+			ImGui::Separator();
+			ImGui::Dummy(ImVec2(7, 7));
+
+			ImGui::Checkbox("Hide Radar During Meetings", &State.HideRadar_During_Meetings);
+			ImGui::Checkbox("Lock Radar Position", &State.LockRadar);
+			SteppedSliderFloat("Radar Opacity", &State.SelectedColor.w, 0.0f, 1.0f, 0.01f, "%.2f Opacity", ImGuiSliderFlags_Logarithmic | ImGuiSliderFlags_NoInput);
 
 			ImGui::EndTabItem();
 		}
