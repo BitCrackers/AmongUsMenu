f9a5a53e182d4e37071945ab4ca23600d99fa936
xAffan <59138135+xAffan@users.noreply.github.com>
2020-12-05 15:46:57 +0500

    Revert "fixed Enter/ExitVent translations (#45) (#7)"
    This reverts commit 5a4c3943f2a9e1f3c77b473898f49af34e099c2d.

--- appdata/il2cpp-translations.h
+++ appdata/il2cpp-translations.h
@@ -70,8 +70,8 @@ const std::vector<std::pair<std::string, std::string>> METHOD_TRANSLATIONS = {
 	{ "get_AmBanned", "PCJCFCBHJDA" },
 	{ "get_BanPoints", "OBAPAPFDDHM" },
 	{ "Deserialize", "GOCFDAMBMLH" },
-	{ "EnterVent", "FLENNEBBHOK" },
-	{ "ExitVent", "EKDCGJEACKM" },
+	{ "EnterVent", "EKDCGJEACKM" },
+	{ "ExitVent", "PLMCPPAGKPH" },
 	{ "ReportDeadBody", "LFFBFCHAJFB" },
 	{ "ShouldFlipSkeld", "FBNFKGDCOAI"}
 };
