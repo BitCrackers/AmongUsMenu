c47e94cd8bb50839e2597e396339fe86cb7d3033
manianac <manianac@gmail.com>
2020-12-05 14:16:12 -0500

    Don't show unload dll button when loaded by xinput

--- gui/tabs/debug_tab.cpp
+++ gui/tabs/debug_tab.cpp
@@ -4,10 +4,12 @@ namespace DebugTab {
 
 	void Render() {
 		if (ImGui::BeginTabItem("Debug")) {
+#ifndef _XINPUT
 			if (ImGui::Button("Unload DLL"))
 			{
 				SetEvent(hUnloadEvent);
 			}
+#endif
 			ImGui::EndTabItem();
 		}
 	}
