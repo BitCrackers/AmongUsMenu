4f0cd51d504bd85218ecad5ee71a5146d5b45a8b
std-nullptr <std-nullptr@protonmail.com>
2020-12-10 21:11:29 -0600

    Update il2cpp-types and il2cpp-api-functions

--- appdata/il2cpp-api-functions.h
+++ appdata/il2cpp-api-functions.h
@@ -1,5 +1,5 @@
 // Generated C++ file by Il2CppInspector - http://www.djkaty.com - https://github.com/djkaty
-// Target Unity version: 2019.4.10 - 2019.4.11
+// Target Unity version: 2019.4.0 - 2019.4.9
 
 #ifndef DO_API_NO_RETURN
 
