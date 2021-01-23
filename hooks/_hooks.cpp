492bbef0c42f6eea8ff0728dc14c826fc7caf2ef
Quentin Ormancey <23664359+louga31@users.noreply.github.com>
2021-01-23 17:04:56 +0100

    Make UI Stream Proof (#118)
    * Make UI Stream Proof
    
    * Implemented sigscan for onPresent
    * Implemented Failback if steam overlay function not found
    
    * Migrated to std-nullptr & void's sigscanner
    
    Added option if user doesn't want to render the cheat, it unloads if not a version proxy.
    
    Co-authored-by: manianac <manianac@gmail.com>

--- hooks/_hooks.cpp
+++ hooks/_hooks.cpp
@@ -3,6 +3,8 @@
 #include "detours/detours.h"
 #include "DirectX.h"
 #include <iostream>
+#include "main.h"
+#include "SignatureScan.hpp"
 
 bool HookFunction(PVOID* ppPointer, PVOID pDetour, const char* functionName) {
 	if (DetourAttach(ppPointer, pDetour) != 0) {
@@ -35,7 +37,18 @@ void DetourInitilization() {
 		std::cout << "Unable to retrieve IDXGISwapChain::Present method" << std::endl;
 		return;
 	} else {
-		oPresent = d3d11.presentFunction;
+		oPresent = SignatureScan<D3D_PRESENT_FUNCTION>("55 8B EC 53 8B 5D ? F6 C3 01 74 ? 53 FF 75 ? FF 75 ? FF 15 ? ? ? ? 5B 5D C2", GetModuleHandleA("GameOverlayRenderer.dll"));
+		if (!oPresent)
+		{
+			if (MessageBox(0, L"Failed to hook the Steam overlay D3DPresent function.  This may cause the menu to be visible to streaming applications.  Do you wish to continue?", L"Error", MB_YESNO | MB_ICONERROR) == IDNO)
+			{
+#ifndef _VERSION
+				SetEvent(hUnloadEvent); //Might as well unload the DLL if we're not going to render anything
+#endif
+				return;
+			}
+			oPresent = d3d11.presentFunction;
+		}
 	}
 
 	HOOKFUNC(SceneManager_Internal_ActiveSceneChanged);
