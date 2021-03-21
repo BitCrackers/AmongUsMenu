f2816c94b961c464c8d274cf4631c3723bb0e45a
Axel <58704636+Suwup@users.noreply.github.com>
2021-03-21 14:47:31 +0100

    Fixed esp scaling to camera height even if zoom is disabled + Disconnected players being rendered (#143)
    * Fixed esp scaling to camera height even if zoom is disabled
    
    * Using Game::pLocalPlayer instead of getting localPlayer in WorldToScreen (This was already commited by manianac but it was accidentally overwritten).
    
    * Removed unnecessary parenthesis in esp.hpp
    
    * Moved lock guard + esp code into scope so that it wont mess up future additions

--- hooks/PlayerControl.cpp
+++ hooks/PlayerControl.cpp
@@ -116,24 +116,27 @@ void dPlayerControl_FixedUpdate(PlayerControl* __this, MethodInfo* method) {
 
 			Transform_set_position(cameraTransform, { playerVector2.x, playerVector2.y, 100 }, NULL);
 		}
+    
+		// We should have this in a scope so that the lock guard only locks the right things
+		{
+			Vector2 localPos = PlayerControl_GetTruePosition(*Game::pLocalPlayer, nullptr);
+			ImVec2 localScreenPosition = WorldToScreen(localPos);
 
-		Vector2 localPos = PlayerControl_GetTruePosition(*Game::pLocalPlayer, nullptr);
-		ImVec2 localScreenPosition = WorldToScreen(localPos);
+			Vector2 playerPos = PlayerControl_GetTruePosition(__this, nullptr);
 
-		Vector2 playerPos = PlayerControl_GetTruePosition(__this, nullptr);
+			PlayerData espPlayerData;
+			espPlayerData.Position = WorldToScreen(playerPos);
+			espPlayerData.Color = AmongUsColorToImVec4(GetPlayerColor(playerData->fields.ColorId));
+			espPlayerData.Name = convert_from_string(playerData->fields.PlayerName);
+			espPlayerData.OnScreen = IsWithinScreenBounds(playerPos);
+			espPlayerData.Distance = Vector2_Distance(localPos, playerPos, nullptr);
+			espPlayerData.playerData = PlayerSelection(__this);
 
-		PlayerData espPlayerData;
-		espPlayerData.Position = WorldToScreen(playerPos);
-		espPlayerData.Color = AmongUsColorToImVec4(GetPlayerColor(playerData->fields.ColorId));
-		espPlayerData.Name = convert_from_string(playerData->fields.PlayerName);
-		espPlayerData.OnScreen = IsWithinScreenBounds(playerPos);
-		espPlayerData.Distance = Vector2_Distance(localPos, playerPos, nullptr);
-		espPlayerData.playerData = PlayerSelection(__this);
-
-		drawing_t& instance = Esp::GetDrawing();
-		std::lock_guard<std::mutex> lock(instance.m_DrawingMutex);
-		instance.LocalPosition = localScreenPosition;
-		instance.m_Players[playerData->fields.PlayerId] = espPlayerData;
+			drawing_t& instance = Esp::GetDrawing();
+			std::lock_guard<std::mutex> lock(instance.m_DrawingMutex);
+			instance.LocalPosition = localScreenPosition;
+			instance.m_Players[playerData->fields.PlayerId] = espPlayerData;
+		}
 
 		// TODO: Improve performance
 		/*Vector2 position = PlayerControl_GetTruePosition(__this, NULL);
