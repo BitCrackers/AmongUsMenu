9ab7c871e304ecfb808e8771e12d3f569f104775
manianac <manianac@gmail.com>
2021-03-23 20:09:52 -0400

    Don't judge me, but this fixes the UI

--- hooks/Camera.cpp
+++ hooks/Camera.cpp
@@ -1,10 +1,18 @@
 #include "pch-il2cpp.h"
 #include "_hooks.h"
+#include "state.hpp"
+#include "esp.hpp"
 #include <iostream>
 
 Vector3 dCamera_ScreenToWorldPoint(Camera* __this, Vector3 position, MethodInfo* method)
 {
-	app::Vector3 ret = app::Camera_ScreenToWorldPoint(__this, position, method);
-	
-	return ret;
+	if (State.EnableZoom)
+	{
+		app::Camera_set_orthographicSize(__this, 3.0f, NULL);
+		app::Vector3 ret = app::Camera_ScreenToWorldPoint(__this, position, method);
+		app::Camera_set_orthographicSize(__this, State.CameraHeight, NULL);
+		return ret;
+	}
+
+	return app::Camera_ScreenToWorldPoint(__this, position, method);
 }
