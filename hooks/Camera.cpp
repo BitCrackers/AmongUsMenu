53f369b1d9e061880131f70d7cba0326da9614b6
OsOmE1 <osome1@protonmail.com>
2021-03-24 18:37:20 +0100

    Remove app:: prefix

--- hooks/Camera.cpp
+++ hooks/Camera.cpp
@@ -8,11 +8,11 @@ Vector3 dCamera_ScreenToWorldPoint(Camera* __this, Vector3 position, MethodInfo*
 {
 	if (State.EnableZoom)
 	{
-		app::Camera_set_orthographicSize(__this, 3.0f, NULL);
-		app::Vector3 ret = app::Camera_ScreenToWorldPoint(__this, position, method);
-		app::Camera_set_orthographicSize(__this, State.CameraHeight, NULL);
+		Camera_set_orthographicSize(__this, 3.0f, NULL);
+		Vector3 ret = Camera_ScreenToWorldPoint(__this, position, method);
+		Camera_set_orthographicSize(__this, State.CameraHeight, NULL);
 		return ret;
 	}
 
-	return app::Camera_ScreenToWorldPoint(__this, position, method);
+	return Camera_ScreenToWorldPoint(__this, position, method);
 }
