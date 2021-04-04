b360adf0098a44dc349b35e3144ae2ee37a4fa85
OsOmE1 <osome1@protonmail.com>
2021-04-04 19:41:18 +0200

    Added check to name setting

--- gui/tabs/settings_tab.cpp
+++ gui/tabs/settings_tab.cpp
@@ -30,7 +30,7 @@ namespace SettingsTab {
 
 			char* nameBuffer[15]{ const_cast<char*>(State.userName.c_str()) };
 			if (ImGui::InputText("PlayerName", *nameBuffer, IM_ARRAYSIZE(nameBuffer))) {
-				State.userName = std::string(*nameBuffer);
+				if (!(IsInGame() || IsInLobby())) State.userName = std::string(*nameBuffer);
 			}
 
 			ImGui::EndTabItem();
