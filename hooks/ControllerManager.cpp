303900d7088642171ead6acf2917ed0b6c060459
manianac <manianac@gmail.com>
2021-04-12 16:17:46 -0400

    Stepping through ui stack to close on meeting conclusion

--- hooks/ControllerManager.cpp
+++ hooks/ControllerManager.cpp
@@ -6,17 +6,20 @@
 void dControllerManager_CloseOverlayMenu(ControllerManager* __this, String* menuName, MethodInfo* method)
 {
 	auto currentMenuName = __this->fields.CurrentUiState->fields.MenuName;
-	/*
+	
 	std::vector<ControllerUiElementsState*> uiStack = GetElementsFromList<List_1_ControllerUiElementsState_*, ControllerUiElementsState*>(__this->fields.CurrentUiStateStack);
-	STREAM_DEBUG("Ui Stack Count: " << uiStack.size());
+	/*STREAM_DEBUG("Ui Stack Count: " << uiStack.size());
 	for (ControllerUiElementsState* uiElement : uiStack)
 	{
 		STREAM_DEBUG("Ui Stack " << convert_from_string(uiElement->fields.MenuName));
-	}
-	*/
-	if (convert_from_string(menuName) == "MeetingHub(Clone)" && convert_from_string(currentMenuName) == "PlayerButton(Clone)")
+	}*/
+	
+	if (convert_from_string(menuName) == "MeetingHub(Clone)" &&
+		(convert_from_string(currentMenuName) != "MeetingHub(Clone)" || !uiStack.empty()))
 	{
-		LOG_INFO("Close PlayerButton before MeetingHub to fix Innersloth movement bug");
+		LOG_INFO("Close UI stack before MeetingHub to fix Innersloth movement bug");
+		for (ControllerUiElementsState* uiElement : uiStack)
+			ControllerManager_CloseOverlayMenu(__this, uiElement->fields.MenuName, method);
 		ControllerManager_CloseOverlayMenu(__this, currentMenuName, method);
 	}
 	ControllerManager_CloseOverlayMenu(__this, menuName, method);
