904ce824570adfb99328bb8714e78a2a2cc22f8c
manianac <manianac@gmail.com>
2021-04-11 22:45:27 -0400

    More specific bug fix conditions

--- hooks/ControllerManager.cpp
+++ hooks/ControllerManager.cpp
@@ -4,10 +4,13 @@
 
 void dControllerManager_CloseOverlayMenu(ControllerManager* __this, String* menuName, MethodInfo* method)
 {
+	auto currentMenuName = __this->fields.CurrentUiState->fields.MenuName;
+
+	//This bug is present as Innersloth somehow cloned MeetingHub, and it does not reset controller input upon closing
 	ControllerManager_CloseOverlayMenu(__this, menuName, method);
-	if (convert_from_string(menuName) == "MeetingHub(Clone)")
+	if (convert_from_string(menuName) == "ChatUi" && convert_from_string(currentMenuName) == "MeetingHub(Clone)")
 	{
+		LOG_INFO("Reset ControllerManager Input to fix Innersloth movement bug");
 		ControllerManager_ResetAll(__this, NULL);
-		LOG_INFO("Reset ControllerManager to fix Innersloth movement bug");
 	}
 }
