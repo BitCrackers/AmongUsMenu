00527d17302c7b362ba9602319114fe397819fab
manianac <manianac@gmail.com>
2021-04-11 22:31:04 -0400

    Add message for fixing innersloth bug

--- hooks/ControllerManager.cpp
+++ hooks/ControllerManager.cpp
@@ -4,10 +4,10 @@
 
 void dControllerManager_CloseOverlayMenu(ControllerManager* __this, String* menuName, MethodInfo* method)
 {
-	auto strMenuName = convert_from_string(menuName);
-	STREAM_DEBUG("Closing " << strMenuName);
-	
 	ControllerManager_CloseOverlayMenu(__this, menuName, method);
-	if (strMenuName == "MeetingHub(Clone)")
+	if (convert_from_string(menuName) == "MeetingHub(Clone)")
+	{
 		ControllerManager_ResetAll(__this, NULL);
+		LOG_INFO("Reset ControllerManager to fix Innersloth movement bug");
+	}
 }
