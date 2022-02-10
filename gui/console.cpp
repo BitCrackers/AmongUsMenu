f663183cac6e2740a529e9408bc28e8695eddcb3
void* <voidp@protonmail.com>
2022-02-10 18:36:35 +0100

    -filter event types the same as player filter -minor changes to console looks

--- gui/console.cpp
+++ gui/console.cpp
@@ -7,7 +7,7 @@
 namespace ConsoleGui
 {
 	// TODO: improve this by building it dynamically based on the EVENT_TYPES enum
-	std::vector<std::pair<const char*, bool>> exclude_filter =
+	std::vector<std::pair<const char*, bool>> event_filter =
 	{
 		{"Kill", false},
 		{"Vent", false},
@@ -44,39 +44,52 @@ namespace ConsoleGui
 
 		ImGui::Begin("Console", &State.ShowConsole, ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoScrollbar);
 		ImGui::BeginChild("console#filter", ImVec2(520, 20), true);
-		ImGui::SameLine(10.f);
-		ImGui::Text("Exclude: ");
+		ImGui::Text("Event Filter: ");
 		ImGui::SameLine();
-		CustomListBoxIntMultiple("Event Types", &ConsoleGui::exclude_filter, 100.f);
+		CustomListBoxIntMultiple("Event Types", &ConsoleGui::event_filter, 100.f);
 		if (IsInGame()) {
-			ImGui::SameLine(0.f, 25.f);
-			ImGui::Text("Filter: ");
+			ImGui::SameLine(0.f, 5.f);
+			ImGui::Text("Player Filter: ");
 			ImGui::SameLine();
 			CustomListBoxPlayerSelectionMultiple("Players", &ConsoleGui::player_filter, 150.f);
 		}
 		ImGui::EndChild();
 		ImGui::Separator();
-		ImGui::BeginChild("console#scroll", ImVec2(490, 225), true);
+		ImGui::BeginChild("console#scroll", ImVec2(511, 275), true, ImGuiWindowFlags_AlwaysVerticalScrollbar);
 		for (int i = State.consoleEvents.size() - 1; i >= 0; i--) {
-			if (State.consoleEvents[i]->getType() == EVENT_WALK
-				|| ConsoleGui::exclude_filter.at(State.consoleEvents[i]->getType()).second)
+			if (State.consoleEvents[i]->getType() == EVENT_WALK)
 				continue;
 
-			bool playerFound = false, anyFilterSelected = false;
+			bool typeFound = false, anyTypeFilterSelected = false;
+			for (int n = 0; n < ConsoleGui::event_filter.size(); n++) {
+				if (ConsoleGui::event_filter[n].second
+					&& (EVENT_TYPES)n == State.consoleEvents[i]->getType()) {
+					typeFound = true;
+					anyTypeFilterSelected = true;
+					break;
+				}
+				else if (ConsoleGui::event_filter[n].second)
+					anyTypeFilterSelected = true;
+			}
+
+			if (!typeFound && anyTypeFilterSelected)
+				continue;
+
+			bool playerFound = false, anyPlayerFilterSelected = false;
 			for (auto player : ConsoleGui::player_filter) {
 				if (player.second
 					&& player.first.has_value()
 					&& player.first.get_PlayerId() == State.consoleEvents[i]->getSource().playerId)
 				{
 					playerFound = true;
-					anyFilterSelected = true;
+					anyPlayerFilterSelected = true;
 					break;
 				}
 				else if (player.second) // if no player was selected we want to make sure that any filter was set in the first place before we continue
-					anyFilterSelected = true;
+					anyPlayerFilterSelected = true;
 			}
 
-			if (!playerFound && anyFilterSelected)
+			if (!playerFound && anyPlayerFilterSelected)
 				continue;
 
 			State.consoleEvents[i]->ColoredEventOutput();
