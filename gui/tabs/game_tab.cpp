0a7a9ee7964bd06003b3ce0a589845d3808ae0c4
kotae4 <83557652+kotae4@users.noreply.github.com>
2022-04-06 00:32:42 -0400

    print lobby settings in game tab (#334)
    * print lobby settings in game tab
    
    * closes BitCrackers/AmongUsMenu#301
    
    * Squashed commit of the following:
    
    commit fb0d1d75c3bcffb13b321b4be5f1e6fbe858901c
    Author: 景大侠 <cddjr@users.noreply.github.com>
    Date:   Tue Apr 5 18:47:48 2022 +0800
    
        #335: fixed a crash when the menu wasn't fully displayed (#336)
    
        The return value of ImGui::ListBoxHeader cannot be ignored
    
    commit ad594e2195e3976d2fa8ffbd7320e324ffc611bc
    Author: kotae4 <83557652+kotae4@users.noreply.github.com>
    Date:   Tue Apr 5 06:45:19 2022 -0400
    
        fixes report event position in replay (#333)
    
        * GetTargetPosition() is where the ghost of the dead player is, so we don't want that
    
    commit d0c536175a060123a1f8361c575d9b6c51e1c848
    Author: 景大侠 <cddjr@users.noreply.github.com>
    Date:   Tue Apr 5 13:30:24 2022 +0800
    
        #234: Scale Menu by dpi (#329)
    
        * Adjust by DPI
    
        * Fix ImGuiWindow::CalcFontSize calculation error
    
        * Adjust by DPI
    
        * replay done
    
        * radar done
    
        * esp done
    
        * added ability to debug dpi scaling
    
    * scale by dpi

--- gui/tabs/game_tab.cpp
+++ gui/tabs/game_tab.cpp
@@ -81,6 +81,53 @@ namespace GameTab {
 				State.rpcQueue.push(new RpcUsePlatform());
 			}
 
+			if (Game::pGameOptionsData != NULL)
+			{
+				ImGui::Dummy(ImVec2(7, 7) * State.dpiScale);
+				ImGui::Separator();
+				ImGui::Dummy(ImVec2(7, 7) * State.dpiScale);
+
+				app::GameOptionsData* options = *Game::pGameOptionsData;
+				if (options != NULL)
+				{
+					auto allPlayers = GetAllPlayerControl();
+					RoleRates roleRates = RoleRates(options->fields, allPlayers.size());
+					// this should be all the major ones. if people want more they're simple enough to add.
+					ImGui::Text("Visual Tasks: %s", (options->fields.VisualTasks == true ? "on" : "off"));
+					ImGui::Text("Taskbar Update Mode: %s", (options->fields.TaskBarMode == app::TaskBarMode__Enum::Normal ? "Normal" : "MeetingOnly"));
+					ImGui::Text("Confirm Impostor: %s", (options->fields.ConfirmImpostor == true ? "on" : "off"));
+					ImGui::Text("Kill CD: %.2f", options->fields._.killCooldown);
+
+					ImGui::Dummy(ImVec2(3, 3) * State.dpiScale);
+					ImGui::Separator();
+					ImGui::Dummy(ImVec2(3, 3) * State.dpiScale);
+
+					ImGui::Text("Max Engineers: %d", roleRates.GetRoleCount(app::RoleTypes__Enum::Engineer));
+					ImGui::Text("Engineer CD: %.2f", options->fields.RoleOptions->fields.EngineerCooldown);
+					ImGui::Text("Engineer Duration: %.2f", options->fields.RoleOptions->fields.EngineerInVentMaxTime);
+
+					ImGui::Dummy(ImVec2(3, 3) * State.dpiScale);
+					ImGui::Separator();
+					ImGui::Dummy(ImVec2(3, 3) * State.dpiScale);
+
+					ImGui::Text("Max Angels: %d", roleRates.GetRoleCount(app::RoleTypes__Enum::GuardianAngel));
+					ImGui::Text("Angel CD: %.2f", options->fields.RoleOptions->fields.GuardianAngelCooldown);
+					ImGui::Text("Angel Duration: %.2f", options->fields.RoleOptions->fields.ProtectionDurationSeconds);
+
+					ImGui::Dummy(ImVec2(3, 3) * State.dpiScale);
+					ImGui::Separator();
+					ImGui::Dummy(ImVec2(3, 3) * State.dpiScale);
+
+					ImGui::Text("Max Shapeshifters: %d", roleRates.GetRoleCount(app::RoleTypes__Enum::Shapeshifter));
+					ImGui::Text("Shapeshifter CD: %.2f", options->fields.RoleOptions->fields.ShapeshifterCooldown);
+					ImGui::Text("Shapeshifter Duration: %.2f", options->fields.RoleOptions->fields.ShapeshifterDuration);
+
+					ImGui::Dummy(ImVec2(3, 3) * State.dpiScale);
+					ImGui::Separator();
+					ImGui::Dummy(ImVec2(3, 3) * State.dpiScale);
+				}
+			}
+
 			ImGui::EndTabItem();
 		}
 	}
