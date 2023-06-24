5e6c3e722418eaaf9a5ef025317f4b82d0c61193
kotae4 <83557652+kotae4@users.noreply.github.com>
2023-06-24 14:08:03 -0400

    pin project to cpp20 (#542)

--- framework/il2cpp-helpers.h
+++ framework/il2cpp-helpers.h
@@ -14,6 +14,10 @@ void output_class_methods(Il2CppClass* klass);
 void output_assembly_methods(const Il2CppAssembly* assembly);
 bool cctor_finished(Il2CppClass* klass);
 
+// NOTE ported this from c++23 so we can pin the project to c++20
+template <class _Ty>
+inline constexpr bool is_scoped_enum_v = std::conjunction_v<std::is_enum<_Ty>, std::negation<std::is_convertible<_Ty, int>>>;
+
 namespace app {
 	namespace il2cpp {
 		template<typename E>
@@ -35,7 +39,7 @@ namespace app {
 			}
 			constexpr iterator end() const { return begin() + size(); }
 			constexpr pointer operator[](const key_type& _Keyval) const {
-				static_assert(std::is_arithmetic_v<key_type> || std::is_scoped_enum_v<key_type>);
+				static_assert(std::is_arithmetic_v<key_type> || is_scoped_enum_v<key_type>);
 				if (!_Ptr) return nullptr;
 				const auto FindEntryMethod = ((System_Collections_Generic_Dictionary_TKey__TValue__RGCTXs*)(_Ptr->klass->rgctx_data))
 					->_17_System_Collections_Generic_Dictionary_TKey__TValue__FindEntry;
