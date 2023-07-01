a9d5b4591cf2f6315c90c6981be0216f07650acb
Dasupergrasskakjd <ethankahoot302@gmail.com>
2023-07-01 19:32:53 +0100

    Displays a message box when detected AUM player

--- rpc/CustomRpcHandler.cpp
+++ rpc/CustomRpcHandler.cpp
@@ -3,6 +3,7 @@
 #include "game.h"
 #include "state.hpp"
 #include "logger.h"
+#include "utility.h"
 
 void HandleRpc(uint8_t callId, MessageReader* reader) {
 	switch (callId) {
@@ -12,6 +13,8 @@ void HandleRpc(uint8_t callId, MessageReader* reader) {
 		uint8_t playerid = MessageReader_ReadByte(reader, NULL);
 		if (!std::count(State.aumUsers.begin(), State.aumUsers.end(), playerid)) {
 			State.aumUsers.push_back(playerid);
+			GameData_PlayerInfo* playerinfo = GetPlayerDataById((Game::PlayerId)playerid);
+			MessageBoxA(NULL, std::format("Received AUM player {}! Use AUM chat to talk with them!", app::GameData_PlayerInfo_get_PlayerName(playerinfo, NULL)).c_str(), "AUM player detected", MB_OK | MB_ICONEXCLAMATION);
 			STREAM_DEBUG("RPC Received for another AUM User from " << ToString((Game::PlayerId)playerid));
 		}
 	}
